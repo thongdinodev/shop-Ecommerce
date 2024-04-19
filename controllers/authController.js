ee8e344ddb5e2bae9a886952d17ab92efa3d6cd1
Thong Dino <hodinhthong.auto@gmail.com>
2024-04-19 16:16:01 +0700

    build fn resetPassword

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const AppError = require('../utils/appError');
 const sendEmail = require('../utils/email');
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 const signToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET_KEY, {
@@ -100,6 +101,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
             subject: 'Your password reset token (valid for 10 min)',
             message
         });
+
+        res.status(200).json({
+            status: 'success',
+            message: 'Token sent to email'
+        })
     } catch (error) {
         user.passwordResetToken = undefined;
         user.passwordResetExpires = undefined;
@@ -110,5 +116,30 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 });
 
 exports.resetPassword = catchAsync(async (req, res, next) => {
+    const hashedToken = crypto
+        .createHash('sha256')
+        .update(req.params.token)
+        .digest('hex');
+    
+    const user = await User.findOne({
+        passwordResetToken: hashedToken,
+        passwordResetExpires: { $gt: Date.now() }
+    });
 
+    if (!user) {
+        return next(new AppError('Token is invalid or has expired', 400));
+    };
+
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save();
+
+    const token = signToken(user._id);
+
+    res.status(200).json({
+        status: 'success',
+        token
+    });
 });
