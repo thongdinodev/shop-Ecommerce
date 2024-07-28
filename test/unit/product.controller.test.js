1ec5127fca7aaa8cf3a14381f6415dc700064957
Thong Dino <hodinhthong.auto@gmail.com>
2024-07-28 20:09:12 +0700

    unit test: updateProduct and deleteProduct

--- test/unit/product.controller.test.js
+++ test/unit/product.controller.test.js
@@ -17,6 +17,61 @@ beforeEach(() => {
     next = null
 })
 
+describe('productController.deleteProduct', () => {
+    it('should have a deleteProduct function', () => {
+        expect(typeof productController.deleteProduct).toBe('function')
+    })
+    it('should delete with productModel.findByIdAndDelete', async () => {
+        req.params.productId = productId
+        await productController.deleteProduct(req, res, next)
+        expect(productModel.findByIdAndDelete).toBeCalledWith(productId)
+    })
+    it('should return nothing and response http code 204', async () => {
+        productModel.findByIdAndDelete.mockReturnValue(newProduct)
+        await productController.deleteProduct(req, res, next)
+        expect(res.statusCode).toBe(204)
+        expect(res._getJSONData()).toStrictEqual(returnProduct)
+        expect(res._isEndCalled()).toBeTruthy()
+    })
+    it('should return 404 if product does not exist to delete', async () => {
+        productModel.findByIdAndDelete.mockReturnValue(null)
+        await productController.deleteProduct(req, res, next)
+        expect(res.statusCode).toBe(404)
+        expect(res._isEndCalled()).toBeTruthy()
+    })
+})
+
+describe('productController.updateProduct', () => {
+    it('should have a updateProduct function', () => {
+        expect(productController.updateProduct).toBe('function')
+    })
+    it('should update with productModel.findByIdAndUpdate', async () => {
+        req.params.productId = productId
+        req.body = newProduct
+        await productController.updateProduct(req, res, next)
+        
+        expect(productModel.findByIdAndUpdate).toHaveBeenCalledWith(productId, newProduct, {
+            new: true,
+            useFindAndModify: false
+        })
+    })
+    it('should return a response with json data and http status code 200', async () => {
+        req.params.productId = productId
+        req.body = newProduct
+        productModel.findByIdAndUpdate.mockReturnValue(newProduct)
+        await productController.updateProduct(req, res, next)
+        expect(res._isEndCalled()).toBeTruthy()
+        expect(res.statusCode).toBe(200)
+        expect(res._getJSONData()).toStrictEqual(returnProduct)
+    })
+    it('should return 404 if product does not exist to update', async () => {
+        productModel.findByIdAndUpdate.mockReturnValue(null)
+        await productController.updateProduct(req, res, next)
+        expect(res.statusCode).toBe(404)
+        expect(res._isEndCalled).toBeTruthy()
+    })
+})
+
 describe('productController.getProduct', () => {
     it('should have a getProduct function', () => {
         expect(typeof productController.getProduct).toBe('function')
@@ -39,7 +94,7 @@ describe('productController.getAllProducts', () => {
     it('should have a getAllProducts function', () => {
         expect(typeof productController.getAllProducts).toBe('function')
     })
-    it('should call todoModel.find', async () => {
+    it('should call productModel.find', async () => {
         req.params.productId = productId
         await productController.getAllProducts(req, res, next)
         expect(productModel.find).toBeCalledWith({})
